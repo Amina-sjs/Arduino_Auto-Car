168697f49f24ba21c9db30f596421036dbc8b62b
Amina <amina.mamadj@gmail.com>
2025-12-04 14:17:36 +0600

    Base setup with motors and servo initialized

---
+++ sketch_apr19a/sketch_apr19a.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+#include <AFMotor.h>
+
+#define Echo A0
+#define Trig A1
+#define motor 10
+#define Speed 170
+#define spoint 103
+
+int distance;
+Servo servo;
+AF_DCMotor M1(1);
+AF_DCMotor M2(2);
+AF_DCMotor M3(3);
+AF_DCMotor M4(4);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+  servo.attach(motor);
+  M1.setSpeed(Speed);
+  M2.setSpeed(Speed);
+  M3.setSpeed(Speed);
+  M4.setSpeed(Speed);
+}
+
+void loop() {
+  Obstacle();
+}
